86649f380d6ce214c8ac27acd593ce4d27ad7273
Nico Moehring <nicomohr@gmx.net>
2015-10-21 01:53:57 +0200

    Added SQL statement

--- src/main/java/com/simpleApplications/audioRecorder/daos/interfaces/IRecordingProjectDao.java
+++ src/main/java/com/simpleApplications/audioRecorder/daos/interfaces/IRecordingProjectDao.java
@@ -1,6 +1,9 @@
 package com.simpleApplications.audioRecorder.daos.interfaces;
 
+import com.simpleApplications.audioRecorder.daos.mappers.RecordingProjectMapper;
 import com.simpleApplications.audioRecorder.model.RecordingProject;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 
 import java.util.List;
 
@@ -8,5 +11,7 @@ import java.util.List;
  * @author Nico Moehring
  */
 public interface IRecordingProjectDao {
+    @SqlQuery("SELECT * FROM recordingProjects ORDER BY id DESC")
+    @Mapper(RecordingProjectMapper.class)
     public List<RecordingProject> getAll();
 }
